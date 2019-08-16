bbc4472f2b1c2b5fc1c5bc1568ef0209ecc25962
Hanna Lee <mani21na@gmail.com>
2019-08-16 23:16:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,10 @@ def monopoly_with_third_tier
               :railroads => 
                             {
                               :pieces => 4,
-                              :names => {},
+                              :names => 
+                                        {
+                                          
+                                        },
                               :rent_in_dollars => 
                                                   {
                                                     :one_piece_owned => 25,                :two_piece_owned => 50,
