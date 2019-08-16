4809d0fc0625e3c9a416f64312626d9636d16cc7
Hanna Lee <mani21na@gmail.com>
2019-08-16 23:16:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,6 +33,7 @@ def monopoly_with_third_tier
                               :names => 
                                         {
                                           :reading_railroad => {},
+                                          :pennsylvania_railroad => {}
                                           
                                         },
                               :rent_in_dollars => 
